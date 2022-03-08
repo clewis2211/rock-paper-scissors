fe58c57ac00e337dc147082ff94ff2201b70f4cc
clewis2211 <carltonlewis42@gmail.com>
2022-03-08 15:56:54 -0600

    init

--- script.js
+++ script.js
@@ -4,31 +4,35 @@ function playGame (playerSelection, computerSelection){
     const paper = ['Paper', 'PAPER', 'pAPER', 'paper'];
     const scissors = ['Scissors', 'SCISSORS', 'sCISSORS', 'scissors']
     
-    // if (startGame == rock){
-    //     console.log(rock[0])
-    //     // return rock;
-    // }else if(startGame == paper) {
-    //     console.log(paper[0])
-    // }else if (startGame == scissors){
-    //     console.log(scissors[0])
-    // }else{
-    //     console.log('choose wisely')
-    // }
+    if (startGame = rock){
+        console.log(rock[])
+        // return rock;
+    }else if(startGame = paper) {
+        console.log(paper)
+    }else if (startGame = scissors){
+        console.log(scissors)
+    }else{
+        console.log('choose wisely')
+    }
 }
 
-// console.loc(playGame())
+playGame()
 
-function computerPlay(){
-    let a = 'Rock';
-    let b = 'Paper';
-    let c = 'Scissors';
+// // console.loc(playGame())
 
-    let playArray = [a, b, c];
-    let randomItem = playArray[Math.floor(Math.random()*playArray.length)]
-    // let randomItem = [Math.floor(Math.random()*playArray.length)]
-    console.log(randomItem)
-}
+// function computerPlay(){
+//     let a = 'Rock';
+//     let b = 'Paper';
+//     let c = 'Scissors';
 
-// const playerSelection ='rock';
-// const computerSelection = computerPlay()
-// console.log(playGame(playerSelection, computerSelection))
+//     let playArray = [a, b, c];
+//     let randomItem = playArray[Math.floor(Math.random()*playArray.length)]
+//     // let randomItem = [Math.floor(Math.random()*playArray.length)]
+//     console.log(randomItem)
+// }
+
+// // const playerSelection ='rock';
+// // const computerSelection = computerPlay()
+// // console.log(playGame(playerSelection, computerSelection))
+
+
